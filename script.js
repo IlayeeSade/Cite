30e832ce7970e76d76d79749fa19bfdee26956f9
Ilayee Sade <ilayeesade@gmail.com>
2024-12-15 16:28:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const details = document.querySelectorAll('details');
-    
-    details.forEach(detail => {
-        detail.addEventListener('toggle', () => {
-            detail.classList.toggle('expanded');
-        });
-    });
-});
